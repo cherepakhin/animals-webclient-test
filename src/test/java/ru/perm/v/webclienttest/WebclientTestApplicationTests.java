db8321dbf82cba5ae71341993775c09d913d54b3
Черепахин Василий <vasi.che@gmail.com>
2023-04-02 20:18:08 +0500

    first commit

---
+++ src/test/java/ru/perm/v/webclienttest/WebclientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.perm.v.webclienttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
